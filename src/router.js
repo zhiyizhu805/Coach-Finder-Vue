4af041173cdf5b27a6e1adf46bae773e15cddd33
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-04 19:18:21 +1300

    bug fixed - curly braces removed

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
-import { createrRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReceived from './pages/requests/RequestReceived.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
-const router = createrRouter({
+
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
@@ -20,7 +21,9 @@ const router = createrRouter({
       ],
     },
     { path: '/register', component: CoachesRegistration },
-    { path: '/requests', component: RequestReceived },
+    { path: '/requests', component: RequestsReceived },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
+
+export default router;
