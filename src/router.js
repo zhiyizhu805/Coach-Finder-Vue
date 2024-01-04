5b7fdea19b36a8065f6a6da4ad85ae0d4a9634e3
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-04 16:24:23 +1300

    update router with corresponding component

--- src/router.js
+++ src/router.js
@@ -1,15 +1,26 @@
-import { createrRouter,createWebHistory } from 'vue-router';
+import { createrRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createrRouter({
-   history: createWebHistory(),
-   routes:[
-    {path:'/',redirect:'/coaches'},
-    {path:'/coaches',component:null},
-    {path:'/coaches/:id',component:null,children:[
-        {path:'contact',component:null} //coaches/c1/contact
-    ]},
-    {path:'/register',component:null},
-    {path:'/requests',component:null},
-    {path:'/:notFound(.*)',component:null},
-   ]
-})
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [
+        { path: 'contact', component: ContactCoach }, //coaches/c1/contact
+      ],
+    },
+    { path: '/register', component: CoachesRegistration },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
