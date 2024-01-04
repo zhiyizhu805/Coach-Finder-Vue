5d4dc7840310c4d4e87afaaceaed90a748d79493
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-04 19:18:02 +1300

    routes updated

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,6 @@
+<template>
+    DETAILS FOR COACH
+    <!-- add a new router-view for child route coach-detail -->
+    <router-view></router-view>
+    <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>FILTER</section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as a Coach</router-link>
+        </div>
+        <ul>LIST OF COACHES</ul>
+    </section>
+    <section>LIST OF COACHES</section>
+</template>
