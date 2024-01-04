732d4da17aafb7df530e7c69bb9f0d0615a30988
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-04 20:43:28 +1300

    coaches list done

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,28 +1,52 @@
 <template>
-    <section>FILTER</section>
-    <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as a Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">{{coach}}</li>
-        </ul>
-        <h3 v-else>No coaches found.</h3>
-    </section>
-    <section>LIST OF COACHES</section>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <!-- <li v-for="coach in filteredCoaches" :key="coach.id"></li> -->
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
+  </section>
+  <section>LIST OF COACHES</section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 // import { mapGetters } from 'vuex'
 export default {
-    computed: {
-        filteredCoaches() {
-            return this.$store.getters['coaches/coaches']
-        },
-        hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches']
-        },
+  components: { CoachItem },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
     },
-}
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
