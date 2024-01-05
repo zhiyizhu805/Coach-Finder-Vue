3187bb133be1cb21c428ee57c9ad804729aa9aa2
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-06 09:52:25 +1300

    loading spinner added

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import store from './pages/store/index.js';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
-
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+import
 const app = createApp( App );
 
 app.use(router);
@@ -14,5 +15,6 @@ app.use(store);
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,8 +6,13 @@
     </section>
     <section>
       <div class="controls">
+        <!-- 7.when click ,call the loadCoaches function -->
         <base-button mode="outeline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach" link  to="/register">Register as a Coach</base-button>
+        <base-button v-if="!isCoach && !isLoading" link  to="/register">Register as a Coach</base-button>
+      </div>
+      <!-- 2.5 register a basespinner globally then use it here -->
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
       </div>
       <ul v-if="hasCoaches">
         <!-- <li v-for="coach in filteredCoaches" :key="coach.id"></li> -->
@@ -36,6 +41,10 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
     return {
+      //(2.1) add a isLoading property to the data() method
+      isLoading: false,
+      //(3.2) add a error property to the data() method
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -59,8 +68,9 @@ export default {
         return false;
       });
     },
+    //(2.6) update the hasCoaches() computed property
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
      isCoach(){
         return this.$store.getters['coaches/isCoach']
@@ -76,8 +86,20 @@ export default {
     },
 
     //5. load all coaches from the server
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      //(2.2) set isLoading to true
+      this.isLoading = true;
+      //(3.3) use try/catch to catch any errors
+      try{
+        //(2.3) this dispatch will return a promise.We can use async/await to wait for the promise to resolve
+        await this.$store.dispatch('coaches/loadCoaches');
+      }catch(error){
+        this.error = error.message || 'Failed to fetch coaches!'
+      }
+
+      //(2.4) set isLoading to false
+      this.isLoading = false;
+
     },
   },
 };
