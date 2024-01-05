72b668246819467eb9b1db5c2096710932903418
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-05 18:47:39 +1300

    contact coach form added

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,3 +1,60 @@
 <template>
-    CONTACT COACH
+  <form>
+    <div class="form-control">
+      <label for="email">Your E-Mail</label>
+      <input type="email" id="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea rows="5" id="message" />
+    </div>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
 </template>
+
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
