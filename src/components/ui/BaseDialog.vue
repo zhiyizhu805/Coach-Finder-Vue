6e4ce80987129692bd7cb27d0de5ecfb165b8c9d
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-06 11:12:17 +1300

    add animation for request model

--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -1,6 +1,8 @@
 <template>
   <teleport to="body">
     <div v-if="show" @click="tryClose" class="backdrop"></div>
+    <!-- (5.1) add transition -->
+    <transition name="dialog">
     <dialog open v-if="show">
       <header>
         <slot name="header">
@@ -16,6 +18,7 @@
         </slot>
       </menu>
     </dialog>
+    </transition>
   </teleport>
 </template>
 
@@ -96,10 +99,32 @@ menu {
   margin: 0;
 }
 
+/* (5.2)add css from transition */
+.dialog-enter-from,
+.dialog-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.dialog-enter-active{
+  transition: all 0.3s ease-out;
+}
+
+.dialog-leave-active{
+  transition: all 0.3s ease-in;}
+
+.dialog-enter-to,
+.dialog-leave-from{
+  opacity: 1;
+  transform: scale(1);
+}
+
 @media (min-width: 768px) {
   dialog {
     left: calc(50% - 20rem);
     width: 40rem;
   }
 }
+
+
 </style>
