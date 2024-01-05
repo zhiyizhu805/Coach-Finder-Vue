a7fddc014268f14526faec77a1263e86d47de1a0
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-05 13:00:41 +1300

    coach register form updated

--- src/pages/coaches/CoachesRegistration.vue
+++ src/pages/coaches/CoachesRegistration.vue
@@ -1,3 +1,16 @@
 <template>
    REGISTER
+   <base-card>
+  <coach-registration></coach-registration>
+   </base-card>
 </template>
+
+<script>
+import CoachRegistration from '../../components/coaches/CoachRegistration.vue'
+export default {
+   components:{
+      CoachRegistration,
+   }
+   
+}
+</script>
