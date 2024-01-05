8b081c18b31d9ff746a4ef244577196fb4305e11
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-06 08:17:11 +1300

    sending register data to firebase done

--- src/pages/store/modules/coaches/actions.js
+++ src/pages/store/modules/coaches/actions.js
@@ -1,14 +1,25 @@
 export default {
-    registerCoach(context, data) {
-        const coachData = {
-            id: context.rootGetters.userId,
-            firstName: data.first,
-            lastName: data.last,
-            areas: data.areas,
-            description: data.desc,
-            hourlyRate: data.rate,
-        };
-        context.commit('registerCoach', coachData);
-    }
-
-}
+    async registerCoach(context, data) {
+      const userId = context.rootGetters.userId
+      const coachData = {
+        firstName: data.first,
+        lastName: data.last,
+        areas: data.areas,
+        description: data.desc,
+        hourlyRate: data.rate,
+      };
+  
+      const response = await fetch(`https://data-c0854-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+        method: 'PUT', //PUT means to override the existing data if it exists// POST means to add new data
+        body: JSON.stringify(coachData),
+      })
+      //   const responseData = await response.json()
+        if(!response.ok) {
+          throw new Error("Failed to register coach")
+        }
+      context.commit('registerCoach', {
+          ...coachData,
+          id: userId});
+    },
+  };
+  
