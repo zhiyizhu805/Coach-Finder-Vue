a7c2e12b51d9e987cbedf4b28f0dead4076289bd
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-05 21:16:23 +1300

    filtering request for logged in coach done

--- src/pages/store/modules/requests/getters.js
+++ src/pages/store/modules/requests/getters.js
@@ -1,8 +1,16 @@
 export default {
-    requests(state) {
-        return state.requests;
-    },
-    hasRequests(state) {
-        return state.requests && state.requests.length > 0;
+    // requests(state) {
+    //     return state.requests;
+    // },
+    // hasRequests(state) {
+    //     return state.requests && state.requests.length > 0;
+    // }
+    //only show requests for the logged in coach
+    requests(state,_,_2,rootGetters) {
+        const coachId = rootGetters.userId;
+        return state.requests.filter(req => req.coachId === coachId);
+    }, 
+    hasRequests(_,getters) {
+        return getters.requests && getters.requests.length > 0;
     }
 }
