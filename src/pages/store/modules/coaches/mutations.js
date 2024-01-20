f5ab6ed4fa79e5374d07bcfdf4193bbd76d0f855
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-21 12:05:46 +1300

    update token

--- src/pages/store/modules/coaches/mutations.js
+++ src/pages/store/modules/coaches/mutations.js
@@ -2,6 +2,7 @@ export default {
     //2. add coach to the local coaches data
     registerCoach(state, payload) {
         state.coaches.push(payload);
+        console.log('statecoaches',state.coaches)
     },
     //4. set local coaches data to the data from firebase
     setCoaches(state, payload) {
