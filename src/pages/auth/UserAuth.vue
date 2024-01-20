73f3cd0d72cef993e6e72e526bcc7be5591792dd
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-20 21:56:02 +1300

    add loading spinner and error handling for signup

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,67 +1,98 @@
 <template>
   <base-card>
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+      <p>
+        {{ error }}
+      </p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
     <form @submit.prevent="submitForm">
       <div class="form-contronl">
         <label for="email">E-mail</label>
-        <input type="email" id="email" v-model="email" @blur="clearState"/>
+        <input type="email" id="email" v-model="email" @blur="clearState" />
       </div>
       <div class="form-control">
         <label for="password">Password</label>
-        <input type="password" id="password" v-model="password" @blur="clearState"/>
+        <input
+          type="password"
+          id="password"
+          v-model="password"
+          @blur="clearState"
+        />
       </div>
-        <p v-if="!formIsValid">Please enter a valid email and password!(Must be at least 6 characters) </p>
-      <base-button>{{submitButtonCaption}}</base-button>
-      <base-button type="button" mode="flat">{{switchModeButtonCaption}}</base-button>
+      <p v-if="!formIsValid">
+        Please enter a valid email and password!(Must be at least 6 characters)
+      </p>
+      <base-button>{{ submitButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchMode">{{
+        switchModeButtonCaption
+      }}</base-button>
     </form>
   </base-card>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-        email: '',
-        password: '',
-        formIsValid: true,
-        mode: 'login'
-        }},
-    computed: {
-        submitButtonCaption(){
-            return this.mode === "login" ? "Login" : "Signup"
-        },
-        switchModeButtonCaption(){
-            return this.mode === "login" ? "Signup instead" : "Login instead"
-        }
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+      isLoading: false,
+      error: null,
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      return this.mode === 'login' ? 'Login' : 'Signup';
     },
-    methods: {
-        clearState(){
-            this.formIsValid = true;
-        },
-        submitForm() {
-            this.formIsValid = true;
-            if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
-                this.formIsValid = false;
-                return;
-            }
-            this.switchMode();
-            if (this.mode === 'login') {
-                // Send a request to login
-
-            } else {
-                // Send a request to signup
-                this.$store.dispatch('signup', {
-                    email: this.email,
-                    password: this.password
-                })
-            }
-        },
-        switchMode() {
-            this.mode = this.mode === 'login' ? 'signup' : 'login';
+    switchModeButtonCaption() {
+      return this.mode === 'login' ? 'Signup instead' : 'Login instead';
+    },
+  },
+  methods: {
+    clearState() {
+      this.formIsValid = true;
+    },
+    async submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      this.isLoading = true;
+      try {
+        if (this.mode === 'login') {
+          // Send a request to login
+        } else {
+          // Send a request to signup
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password,
+          });
+          this.isLoading = false;
         }
-    }
-  }
+      } catch (error) {
+        this.error = error.message || 'An error occured';
+        this.isLoading = false;
+      }
+    },
+    switchMode() {
+      this.mode = this.mode === 'login' ? 'signup' : 'login';
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+};
 </script>
-
 
 <style scoped>
 form {
