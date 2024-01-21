e0971309720d350af6092d6c9cebbc93e3648b42
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-21 15:04:04 +1300

    add navigation guards to protect urls

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
+import store from './pages/store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -21,11 +22,25 @@ const router = createRouter({
         { path: 'contact', component: ContactCoach }, //coaches/c1/contact
       ],
     },
-    { path: '/register', component: CoachesRegistration },
-    { path: '/requests', component: RequestsReceived },
+    //(navigation guard2) add meta data to routes that need to be protected
+    { path: '/register', component: CoachesRegistration,meta:{requiresAuth:true} },
+    { path: '/requests', component: RequestsReceived,meta:{requiresAuth:true} },
+    { path: '/auth', component: UserAuth,meta:{requiresUnauth:true}},
     { path: '/:notFound(.*)', component: NotFound },
-    { path: '/auth', component: UserAuth}
   ],
+});
+
+//(navigation guard1 )create a global navigation guard
+router.beforeEach(function(to, _, next) {
+  //(navigation guard3 )analyze the route we are trying to access, and check if it has the meta data we added
+  // we can access store by importing it
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated){
+    next('/auth');
+}else if(to.meta.requiresUnauth && store.getters.isAuthenticated){
+  next('/coaches');}
+  else{
+    next();
+  }
 });
 
 export default router;
